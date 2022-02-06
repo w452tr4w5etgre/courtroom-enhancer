1ccb3181b6240f2308d487b538fe8fc8a00f9920
w452tr4w5etgre <99140119+w452tr4w5etgre@users.noreply.github.com>
2022-02-06 20:45:39 +0100

    Update courtroomenhancer.user.js

--- courtroomenhancer.user.js
+++ courtroomenhancer.user.js
@@ -2,7 +2,7 @@
 // @name         Objection.lol Courtroom Enhancer
 // @namespace    https://objection.lol/courtroom/*
 // @description  Enhances Objection.lol Courtroom functionality
-// @version      0.35
+// @version      0.36
 // @author       You
 // @match        https://objection.lol/courtroom/*
 // @icon         https://www.google.com/s2/favicons?domain=objection.lol
@@ -19,28 +19,41 @@ var scriptSetting = {
     "evid_roulette": getSetting("evid_roulette", true)
 };
 
+var storedUsername = GM_getValue("courtroom_username","")
+
 window.addEventListener('beforeunload', confirmClose, false);
 
-(new MutationObserver(checkJoinBox)).observe(document, {childList: true, subtree: true});
+(new MutationObserver(checkJoinBoxReady)).observe(document, {childList: true, subtree: true});
 
-function checkJoinBox(changes, observer) {
+function checkJoinBoxReady(changes, observer) {
     // Wait for the Join pop-up to show up
     if(document.querySelector("#app > div.v-dialog__content.v-dialog__content--active > div > div")) {
         observer.disconnect();
-        let username_input = document.querySelector("#app > div.v-dialog__content > div > div > form > div.v-card__text > div > div > div > div > div.v-input__slot > div > input");
-        username_input.value = GM_getValue("courtroom_username","");
-        username_input.dispatchEvent(new Event("input"));
+        let username_join_input = document.querySelector("#app > div.v-dialog__content > div > div > form > div.v-card__text > div > div > div > div > div.v-input__slot > div > input");
+        username_join_input.value = storedUsername;
+        username_join_input.dispatchEvent(new Event("input"));
 
         // When the "Join" button is clicked
         document.querySelector("#app > div.v-dialog__content.v-dialog__content--active > div > div > form > div.v-card__actions > button:nth-child(3) > span").parentNode.addEventListener('click', function(){
-            GM_setValue("courtroom_username", username_input.value);
+            storedUsername = username_join_input.value
+            GM_setValue("courtroom_username", username_join_input.value);
         });
 
         // When "Enter" is pressed in the username input box
-        username_input.addEventListener("keydown", function(e) {
-            if (username_input.value && (e.keyCode == 13 || e.key == "Enter")) {
-                GM_setValue("courtroom_username", username_input.value);
+        username_join_input.addEventListener("keydown", function(e) {
+            if (username_join_input.value && (e.keyCode == 13 || e.key == "Enter")) {
+                storedUsername = username_join_input.value
+                GM_setValue("courtroom_username", username_join_input.value);
             }
+        });
+
+        let username_change_input = document.querySelector("#app > div > div.container > main > div > div > div > div:nth-child(2) > div > div > div > div > div.v-window-item > div > div:nth-child(1) > div > div.v-input > div.v-input__control > div.v-input__slot > div.v-text-field__slot > input[type=text]")
+        username_change_input.addEventListener("focusout", function(e) {
+            // Set a timeout because for some reason the name box reverts for a split second on change
+            setTimeout(function() {
+            storedUsername = username_change_input.value
+            GM_setValue("courtroom_username", username_change_input.value);
+            }, 1000);
         });
 
         // Add setting options under the Settings tab
@@ -94,15 +107,15 @@ function createSettingsElements() {
         return div;
     }
 
-    let extra_warn_on_exit = create_extra_setting_elem("warn_on_exit", "Warn on exit", function(e) {
+    let extra_warn_on_exit = create_extra_setting_elem("warn_on_exit", "Warn on page exit", function(e) {
         let value = e.target.checked;
         setSetting("warn_on_exit", value);
     })
 
-    let extra_evid_roulette = create_extra_setting_elem("evid_roulette", "Evid roulette", function(e) {
+    let extra_evid_roulette = create_extra_setting_elem("evid_roulette", "Evidence roulette button", function(e) {
         let value = e.target.checked;
         setSetting("evid_roulette", value);
-        document.querySelector("div#extra_roulette_button").style.display = getSetting("evid_roulette") ? "block" : "none"
+        document.querySelector("div#extra_roulette_button").style.visibility = getSetting("evid_roulette") ? "visible" : "hidden"
     });
 
     extra_settings_col.append(extra_warn_on_exit, extra_evid_roulette);
@@ -111,36 +124,45 @@ function createSettingsElements() {
 }
 
 function createRouletteButton() {
+    // Upper limit of random evidence number
     var max_evid = 461000;
 
-    let elem_div = document.createElement('div');
-    elem_div.setAttribute('class','pl-1');
+    let elem_div = document.createElement("div");
+    elem_div.setAttribute('class','px-1');
     elem_div.id = "extra_roulette_button"
-    let elem_button = document.createElement('button');
-    elem_button.setAttribute('class','v-btn v-btn--has-bg theme--dark v-size--small primary');
-    elem_button.setAttribute('type','button');
-    elem_button.style = 'background-color: #f37821 !important';
-    let elem_span = document.createElement('span');
-    elem_span.setAttribute('class','v-btn__content');
-    let elem_i=document.createElement('i');
-    elem_i.setAttribute('class','v-icon notranslate mdi theme--dark');
-    elem_i.textContent='EVD';
+    let elem_button = document.createElement("button");
+    elem_button.setAttribute("class","v-btn v-btn--has-bg theme--dark v-size--small primary");
+    elem_button.setAttribute("type","button");
+    elem_button.style = 'background-color: #f37821 !important;';
+    let elem_span = document.createElement("span");
+    elem_span.setAttribute("class","v-btn__content");
+    let elem_i=document.createElement("i");
+    elem_i.setAttribute("class","v-icon notranslate mdi theme--dark");
+    elem_i.textContent = "EVD";
+    elem_i.style.fontSize = "18px";
 
     elem_span.appendChild(elem_i);
     elem_button.appendChild(elem_span);
     elem_div.appendChild(elem_button);
 
     let existing_button = document.querySelector("#app div.v-application--wrap div.pl-1 button");
-    existing_button.parentNode.after(elem_div);
+    existing_button.parentNode.parentNode.firstChild.before(elem_div);
 
     elem_button.onclick = function() {
-        let textarea=document.querySelector("#app > div > div > main > div textarea.frameTextarea");
-        textarea.value='[#evd'+Math.floor(Math.random() * max_evid)+']';
+        let textarea = document.querySelector("#app > div > div > main > div textarea.frameTextarea");
+        let random = Math.floor(Math.random() * max_evid)
+        textarea.value = "[#evd" + random + "]";
         textarea.dispatchEvent(new Event("input"));
+
+        // Click Send button
         document.querySelector("#app > div div div button i.mdi-send").parentNode.parentNode.click();
+
+        // Update textarea value with ID
+        textarea.value = "Last evidence: [#evd" + random + "] (" + String(random) + ")";
+        textarea.dispatchEvent(new Event("input"));
     }
 
-    elem_div.style.display = getSetting("evid_roulette") ? "block" : "none"
+    elem_div.style.visibility = getSetting("evid_roulette") ? "visible" : "hidden";
 }
 
 function confirmClose (zEvent) {
