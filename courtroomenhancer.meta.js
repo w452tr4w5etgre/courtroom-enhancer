4ac89663ebe9e69c24b36adb90412ee17256246a
w452tr4w5etgre <99140119+w452tr4w5etgre@users.noreply.github.com>
2022-02-06 19:35:15 +0100

    Create courtroomenhancer.meta.js

---
+++ courtroomenhancer.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Objection.lol Courtroom Enhancer
+// @namespace    https://objection.lol/courtroom/*
+// @description  Enhances Objection.lol Courtroom functionality
+// @version      0.32
+// @author       You
+// @match        https://objection.lol/courtroom/*
+// @icon         https://www.google.com/s2/favicons?domain=objection.lol
+// @downloadURL  https://gist.github.com/w452tr4w5etgre/def07801904203a5fcd2900472529986/raw/96cec9e819abe12dd072dcd65e84a3bafc107e1c/courtroomenhancer.user.js
+// @updateURL    https://gist.github.com/w452tr4w5etgre/def07801904203a5fcd2900472529986/raw/96cec9e819abe12dd072dcd65e84a3bafc107e1c/courtroomenhancer.meta.js
+// @grant        GM_getValue
+// @grant        GM_setValue
+// ==/UserScript==
