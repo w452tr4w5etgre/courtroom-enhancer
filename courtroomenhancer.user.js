c427642c8dc4f2959d1935f2f7a26be245926dec
w452tr4w5etgre <99140119+w452tr4w5etgre@users.noreply.github.com>
2022-03-20 15:41:47 +0100

    Update courtroomenhancer.user.js

--- courtroomenhancer.user.js
+++ courtroomenhancer.user.js
@@ -2,7 +2,7 @@
 // @name         Objection.lol Courtroom Enhancer
 // @namespace    https://github.com/w452tr4w5etgre/
 // @description  Enhances Objection.lol Courtroom functionality
-// @version      0.717
+// @version      0.718
 // @author       w452tr4w5etgre
 // @homepage     https://github.com/w452tr4w5etgre/courtroom-enhancer
 // @match        https://objection.lol/courtroom/*
