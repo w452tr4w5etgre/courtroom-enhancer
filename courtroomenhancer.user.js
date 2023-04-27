84af2e7899a4bceb65791436924eb199308f0430
w452tr4w5etgre <99140119+w452tr4w5etgre@users.noreply.github.com>
2023-04-28 00:30:53 +0200

    TTS maybe fix

--- courtroomenhancer.user.js
+++ courtroomenhancer.user.js
@@ -2,7 +2,7 @@
 // @name         Objection.lol Courtroom Enhancer
 // @namespace    https://github.com/w452tr4w5etgre/
 // @description  Enhances Objection.lol Courtroom functionality
-// @version      0.854
+// @version      0.855
 // @author       w452tr4w5etgre
 // @homepage     https://github.com/w452tr4w5etgre/courtroom-enhancer
 // @match        https://objection.lol/courtroom/*
@@ -2044,8 +2044,11 @@
 
                 // Filter only English voices
                 this.voices = speechSynthesis.getVoices();
-                speechSynthesis.onvoiceschanged = () => {
-                    this.voices = Object.values(speechSynthesis.getVoices()).filter(voice => voice.lang.startsWith("en-"));
+
+                if (speechSynthesis.onvoiceschanged === null) {
+                    speechSynthesis.onvoiceschanged = () => {
+                        this.voices = Object.values(speechSynthesis.getVoices()).filter(voice => voice.lang.startsWith("en-"));
+                    }
                 }
 
                 this.utterances = {};
